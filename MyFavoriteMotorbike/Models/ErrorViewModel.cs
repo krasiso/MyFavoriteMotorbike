049ee80c419efca933e4a61e42665066557d10d2
Krasimir Nikolov <kraschonikolov@gmail.com>
2022-11-07 09:16:45 +0200

    Add project files.

---
+++ MyFavoriteMotorbike/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFavoriteMotorbike.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
